b98464e9709a08d158ea87203fd4f57817481c07
helanludovic-commits <helan.ludovic@gmail.com>
2025-09-25 09:24:55 +0200

    Update webhooks.js

--- api/webhooks.js
+++ api/webhooks.js
@@ -24,8 +24,8 @@ module.exports = async function handler(req, res) {
     console.log(`[${requestId}] Traitement pour le contact : ${userEmail} de l'agence ${ghlLocationId}`);
 
     const supabaseAdmin = createClient(
-      process.env.SUPABASE_URL,
-      process.env.SUPABASE_SERVICE_KEY
+      process.env.NEXT_PUBLIC_SUPABASE_URL,
+      process.env.SUPABASE_SERVICE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
     );
 
     // --- LOGIQUE MULTI-TENANT ---
